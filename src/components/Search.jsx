489410143f1536076346bd100116754a54603985
= <sravangaming7310@gmail.com>
2022-06-06 13:20:44 +0530

    Convert searched item to lowercase

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Search = (props) => {
   const handleChange = (e) => {
-    props.setPokemon(e.target.value);
+    props.setPokemon(e.target.value.toLowerCase());
   };
 
   return (
